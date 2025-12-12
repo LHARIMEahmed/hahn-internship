3c8db0c65b17acedde88c8ec8eb902adf8a09435
LHARIMEAhmed <ahmedlharime233@gmail.com>
2025-12-12 20:03:11 +0100

    commit project structure

---
+++ backend/src/main/java/com/example/hahn_internship/HahnInternshipApplication.java
@@ -0,0 +1,13 @@
+package com.example.hahn_internship;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HahnInternshipApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HahnInternshipApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/example/hahn_internship/HahnInternshipApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hahn_internship;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HahnInternshipApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
